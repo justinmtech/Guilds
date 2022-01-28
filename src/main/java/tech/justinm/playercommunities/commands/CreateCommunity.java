e50dda4b8e5d0ac5b1ddf62ce26198da09fd5378
JustinM95 <justin.mitchell09@gmail.com>
2022-01-28 09:58:19 -0600

    Added handling for non-unique community names so all names are unique

--- src/main/java/tech/justinm/playercommunities/commands/CreateCommunity.java
+++ src/main/java/tech/justinm/playercommunities/commands/CreateCommunity.java
@@ -17,7 +17,11 @@ public class CreateCommunity extends SubCommand {
     public void execute() {
         Player player = (Player) getSender();
         String name = getArgs()[1];
+        if (getPlugin().getData().getCommunity(name) == null) {
         getPlugin().getData().createCommunity(player.getUniqueId(), name);
         Message.send(getPlugin(), player, "create-community");
+        } else {
+            Message.send(getPlugin(), getSender(), "community-already-exists");
+        }
     }
 }
