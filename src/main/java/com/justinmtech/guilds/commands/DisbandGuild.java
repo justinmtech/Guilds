a6687ec229b146498ca824b593752a1beb4b184e
JustinM95 <justin.mitchell09@gmail.com>
2022-12-09 11:30:34 -0600

    Fixed config value referring to "/pc" command

--- src/main/java/com/justinmtech/guilds/commands/DisbandGuild.java
+++ src/main/java/com/justinmtech/guilds/commands/DisbandGuild.java
@@ -19,6 +19,7 @@ public class DisbandGuild extends SubCommand {
     @SuppressWarnings("UnusedReturnValue")
     public boolean execute() {
         Player player = (Player) getSender();
+        System.out.println(getPlugin().getData().getGuild(player.getUniqueId()));
         try {
             Optional<Guild> guild = getPlugin().getData().getGuild(player.getUniqueId());
             if (guild.isEmpty()) {
